d3b5e3508122f9e61939a80983022399f6a30273
DanielHenriqueSilvaFernadnes <danielhenriquesou@hotmail.com>
2018-08-08 17:11:43 -0300

    Lab1 Finalizado - Falta adcione delete mode

--- Lab1/ListaCandidatos.h
+++ Lab1/ListaCandidatos.h
@@ -16,6 +16,7 @@ class ListaCandidatos{
 		ListaCandidatos(){
 			head = NULL;
 			}
+		
 		ListaCandidatos(string nomeDoArquivo){
 			head = NULL;
 			ifstream fcin(nomeDoArquivo);
@@ -26,12 +27,14 @@ class ListaCandidatos{
 				adicioneComoHead(new Candidato(dados));
 			}
 			}
+		
 		void adicioneComoHead(Candidato* c){
 			head = new NoCandidato(c, head);
 			}
 		bool estaVazia(){
 			return head==NULL;
 			}
+		
 		int tamanho(){
 			int contador = 0;
 			NoCandidato* aux = head;
@@ -41,6 +44,7 @@ class ListaCandidatos{
 			}
 			return contador;
 			}
+			
 		string toString(){
 			if(estaVazia()){
 				return "0";	
@@ -48,23 +52,44 @@ class ListaCandidatos{
 				return head->toString();
 				}		 
 			}
+			
 		bool remove(string nome, string sobrenome){
 			if(tamanho() == 0){
 				
 				return false;
 				}
-			NoCandidato* it = head;
 			if(head->conteudo->igual(nome,sobrenome)){
 				head = head->next;
 				return true;
 				}
+			NoCandidato* it = head;
 			while(it->next!= NULL){
 				if(it->next->conteudo->igual(nome,sobrenome)){
-					it->next = new NoCandidato(it->next->next->conteudo,NULL);
+					//it->next = new NoCandidato(it->next->next->conteudo,NULL);
+					it->next = it->next->next;
 					return true;
 					}
 					it = it->next;
 				}
 			return false;
+			}
+			
+		void filtrarCandidatos(int nota){
+			NoCandidato* it = head;
+			while(it!= NULL){
+				if(it->conteudo->nota < nota){
+					remove(it->conteudo->nome,it->conteudo->sobrenome);
+					}
+					it = it->next;
+				}
+			}
+
+		void concatena(ListaCandidatos* l){
+			NoCandidato* it = head;
+			while(it->next != NULL){
+					it = it->next;
+				}
+				it->next = l->head;
+				//head = it;
 			}
 };
